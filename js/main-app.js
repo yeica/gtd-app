dbd6ed1cf01013e8d324ef41f861bf0b2e2673af
Yeyskalyn <yeyskalyn@gmail.com>
2021-09-23 16:28:41 -0400

    local storage to ideas and profile added

--- js/main-app.js
+++ js/main-app.js
@@ -14,22 +14,21 @@ var mainApp = new Vue({
       profile: {
         name: '',
         avatar: '',
-        color: ''
+        color: "#ffffff"
       },
-      doQuickList: 
-      [
-        {
-          title: 'Hornear Pan',
-          description: 'Con mi novio'
+      doQuickList: [
+        /*{
+          title: 'Comer mucho pan',
+          description: 'porque tengo hambre'
         },
         {
-          title: 'Hacer la tarea',
-          description: 'Con mi novio'
+          title: 'Comer mucha yaroa',
+          description: 'porque tengo hambre'
         },
         {
-          title: 'Comer yaroa',
-          description: 'Con mi novio'
-        }
+          title: 'Comer mucha berenjena',
+          description: 'porque tengo hambre'
+        }*/
       ],
       doLaterList: [],
       waitingList: [],
@@ -48,23 +47,34 @@ var mainApp = new Vue({
         if (this.isActionable == true) {
           if (this.canBeDoneTwoMin == true){
             this.doQuickList.push(idea);
+            localStorage.setItem('doQuickList', JSON.stringify(this.doQuickList));
           }
           else{
-            this.sendActionable == 'toDo' ? this.doLaterList.push(idea) : this.waitingList.push(idea);
+            if (this.sendActionable == 'toDo') {
+              this.doLaterList.push(idea);
+              localStorage.setItem('doLaterList', JSON.stringify(this.doLaterList));
+            }
+            else{
+              this.waitingList.push(idea);
+              localStorage.setItem('waitingList', JSON.stringify(this.waitingList));
+            } 
           }
         }
         else {
           switch (this.sendNonActionable) {
             case 'someDay':
               this.someDayList.push(idea);
+              localStorage.setItem('someDayList', JSON.stringify(this.someDayList));
               break;
 
             case 'references':
               this.referencesList.push(idea);
+              localStorage.setItem('referencesList', JSON.stringify(this.referencesList));
               break;
 
             case 'trash':
               this.trashList.push(idea);
+              localStorage.setItem('trashList', JSON.stringify(this.trashList));
               break;
           
             default:
@@ -155,37 +165,37 @@ var mainApp = new Vue({
               this.titles = {
                 title: 'About doing things quickly',
                 description: 'something',
-              }
+              };
               break;
             case 'doLaterList':
               this.titles = {
                 title: 'About deferring tasks',
                 description: 'something',
-              }
+              };
               break;
             case 'waitingList':
               this.titles = {
                 title: 'About delegating tasks',
                 description: 'something',
-              }
+              };
               break;
             case 'someDayList':
               this.titles = {
                 title: 'About incubating ideas',
                 description: 'something',
-              }
+              };
               break;
             case 'referencesList':
               this.titles = {
                 title: 'About saving ideas as references',
                 description: 'something',
-              }
+              };
               break;
             case 'trashList':
               this.titles = {
                 title: 'About trash ideas',
                 description: 'something',
-              }
+              };
               break;
           
             default:
@@ -205,7 +215,31 @@ var mainApp = new Vue({
         console.log(this.profile.avatar);
       },
       updateProfile: function () {
-        console.log(this.profile);
+        localStorage.setItem('profile', JSON.stringify(this.profile));
+        this.closeModal('updateProfileInfoModal');
+      },
+      updateLocalStorage: function (){
+        //localStorage.setItem('gtdData', JSON.stringify(this.localStorageData));
+        console.log(this.localStorageData)
+      },
+      getLocalStorage: function () {
+        let doQuick = JSON.parse(localStorage.getItem('doQuickList'));
+        let doLater = JSON.parse(localStorage.getItem('doLaterList'));
+        let waiting = JSON.parse(localStorage.getItem('waitingList'));
+        let someDay = JSON.parse(localStorage.getItem('someDayList'));
+        let references = JSON.parse(localStorage.getItem('referencesList'));
+        let trash = JSON.parse(localStorage.getItem('trashList'));
+
+        
+        doQuick == null ? this.doQuickList = [] : this.doQuickList = doQuick;
+        doLater == null ? this.doLaterList = [] : this.doLaterList = doLater;
+        waiting == null ? this.waitingList = [] : this.waitingList = waiting;
+        someDay == null ? this.someDayList = [] : this.someDayList = someDay;
+        references == null ? this.referencesList = [] : this.referencesList = references;
+        trash == null ? this.trashList = [] : this.trashList = trash;
       }
-    }
+    },
+    created: function () {
+      this.getLocalStorage();
+    },
   })
